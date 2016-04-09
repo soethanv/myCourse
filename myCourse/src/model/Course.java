9d93f7ecaae5f11732c59516051c968f0c2b9be9
Ryan Zarmbinski <zvy727@my.utsa.edu>
2016-04-09 03:11:59 -0500

    Added model and constants for course

---
+++ myCourse/src/model/Course.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Course {
+	public static final String COURSE_CS = "CS";
+	public static final String COURSE_BIO = "BIO";
+	public static final String COURSE_MAT = "MAT";
+	
+}
